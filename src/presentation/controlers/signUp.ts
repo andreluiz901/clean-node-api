7cb2da2e027d4cdf195a5498aa9e1ead5d536138
André Luiz <silva.ala82@gmail.com>
2023-12-04 16:27:07 -0300

    refactor: dimplifield code for signUpControlelr

--- src/presentation/controlers/signUp.ts
+++ src/presentation/controlers/signUp.ts
@@ -4,14 +4,11 @@ import { badRequest } from "../helpers/http-helper";
 
 export class SignUpController {
     handle(httpRequest: httpRequest): httpResponse {
-        if (!httpRequest.body.name) {
-            return badRequest(new MissingParamError('name'))
+        const requiredField = ['name', 'email']
+        for (const field of requiredField) {
+            if (!httpRequest.body[field]) {
+                return badRequest(new MissingParamError(field))
+            }
         }
-
-        if (!httpRequest.body.email) {
-            return badRequest(new MissingParamError('email'))
-
-        }
-
     }
 }
